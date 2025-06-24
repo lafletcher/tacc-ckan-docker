c47ccab59502399bdddb71cedf524d528b366905
Maximiliano Osorio <maxiosorio@gmail.com>
2025-06-23 20:31:27 -0400

    feat: add DYNAMO Dashboard URL configuration and helper method for dynamic URL generation in TACC theme

--- src/ckanext-tacc_theme/ckanext/tacc_theme/plugin.py
+++ src/ckanext-tacc_theme/ckanext/tacc_theme/plugin.py
@@ -10,6 +10,7 @@ import re
 
 class TaccThemePlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.ITemplateHelpers)
 
     # IConfigurer
 
@@ -18,12 +19,23 @@ class TaccThemePlugin(plugins.SingletonPlugin):
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('assets', 'tacc_theme')
 
+    # ITemplateHelpers
+
+    def get_helpers(self):
+        return {
+            'get_dynamo_dashboard_url': self.get_dynamo_dashboard_url,
+        }
+
+    def get_dynamo_dashboard_url(self):
+        """Get the DYNAMO Dashboard URL from CKAN configuration"""
+        return toolkit.config.get('ckanext.tacc_theme.dynamo_dashboard_url', 'https://mint.tacc.utexas.edu')
+
     def markdown_extract_paragraphs(text: str, extract_length: int = 190) -> Union[str, Markup]:
-        ''' return the plain text representation of markdown (ie: text without any html tags) 
+        ''' return the plain text representation of markdown (ie: text without any html tags)
         as a list of paragraph strings.'''
         if not text:
             return ''
-        
+
         # find all tags but ignore < in the strings so that we can use it correctly
         # in markdown
         RE_MD_HTML_TAGS = re.compile('<[^><]*>')
