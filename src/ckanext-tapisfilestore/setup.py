523ad215fe92a8c1503ecd63d25980bafc47d255
Maximiliano Osorio <maxiosorio@gmail.com>
2025-06-26 09:24:10 -0400

    fix: correct class name in setup.py for Tapis file store plugin

--- src/ckanext-tapisfilestore/setup.py
+++ src/ckanext-tapisfilestore/setup.py
@@ -84,7 +84,7 @@ setup(
     # pip to create the appropriate form of executable for the target platform.
     entry_points='''
         [ckan.plugins]
-        tapisfilestore=ckanext.tapisfilestore.plugin:TapisfilestorePlugin
+        tapisfilestore=ckanext.tapisfilestore.plugin:TapisFilestorePlugin
 
         [babel.extractors]
         ckan = ckan.lib.extract:extract_ckan
