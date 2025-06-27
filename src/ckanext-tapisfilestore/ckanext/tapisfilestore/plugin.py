fdef6e6e5935901bc5edd20bffc51a7e9b242c77
Maximiliano Osorio <maxiosorio@gmail.com>
2025-06-26 20:19:26 -0400

    fix: streamline unauthorized response handling in TapisFilestorePlugin
    - Updated the serve_file method to return a 401 status without specifying content type for unauthorized access, simplifying the response structure.
    - This change enhances consistency in error messaging and aligns with recent refactoring efforts for improved maintainability.

--- src/ckanext-tapisfilestore/ckanext/tapisfilestore/plugin.py
+++ src/ckanext-tapisfilestore/ckanext/tapisfilestore/plugin.py
@@ -13,7 +13,7 @@ import logging
 import requests
 import mimetypes
 from urllib.parse import quote, unquote
-from flask import Response, stream_with_context
+from flask import Response, stream_with_context, request
 
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
@@ -166,11 +166,10 @@ class TapisFilestorePlugin(plugins.SingletonPlugin):
 
         tapis_token = self._get_tapis_token()
         if not tapis_token:
-            # if accept is html, return a html page
             if request.headers.get('Accept') == 'text/html':
                 return Response('You must be logged in to access this resource. Please log in and try again.', status=200, content_type='text/html')
             else:
-                return Response('You must be logged in to access this resource. Please log in and try again.', status=401, content_type='text/plain')
+                return Response('You must be logged in to access this resource. Please log in and try again.', status=401)
 
         response_file_info = self.request_file_info(file_path, tapis_token)
         if self.intercept_errors(response_file_info.status_code, file_path):
